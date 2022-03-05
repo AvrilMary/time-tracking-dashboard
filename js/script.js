13757717cbde21793fe88ea19087d34aae132969
Avril_M <turvey.am@gmail.com>
2022-03-05 17:01:37 -0600

    JS changes

--- js/script.js
+++ js/script.js
@@ -3,10 +3,18 @@
 
 const daily = document.querySelector(".daily");
 const weekly = document.querySelector(".weekly");
+console.log(weekly);
 const monthly = document.querySelector(".monthly");
 
+//Work
+const currentDayWork = document.querySelector("#work .daily-time");
+console.log(currentDayWork);
+const currentWeekWork = document.querySelector("#work .weekly-time");
+const currentMonthWork = document.querySelector("#work .monthly-time");
 
-
+weekly.addEventListener("click", function () {
+    currentWeekWork.style.color = "purple";
+});
 
 
 
