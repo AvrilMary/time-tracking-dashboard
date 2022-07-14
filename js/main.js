874a15a6175fbe142a8c9df98fe130e0d9e064d3
Avril_M <turvey.am@gmail.com>
2022-07-14 07:30:49 -0500

    Changes to js file

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 
+
+
+const jsonData= require('./data.json'); 
+console.log(jsonData);
+
+
 // Global variables
 
 const daily = document.querySelector(".daily");
@@ -7,8 +13,9 @@ console.log(weekly);
 const monthly = document.querySelector(".monthly");
 
 
+/*
 const getData = async function () {
-    const res = await fetch (`./data.json`);
+    const res = await fetch ("data.json");
     const data = await res.json();
     console.log(data);
 }
@@ -16,4 +23,4 @@ const getData = async function () {
 getData();
 
 
-
+*/
