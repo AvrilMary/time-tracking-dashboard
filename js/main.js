e74881ff9fa043d2a3b29dcb98e1d77c3262c6e0
Avril_M <turvey.am@gmail.com>
2022-07-22 17:48:24 -0500

    Working on accessing properties from JSON data

--- js/main.js
+++ js/main.js
@@ -2,29 +2,33 @@
 // Replace ./data.json with your JSON feed
 
 
-
 fetch('./data.json').then(response => {
     return response.json();
   }).then(data => {
     // Work with JSON data here
-    console.log(data);
+   console.log(data);    
+   const res = JSON.parse(data);
+   console.log(res);
   }).catch(err => {
     // Do something for an error here
   });
 
+  
 
-/*
 
+  /*
 async function getData() {
   let url = './data.json';
   try {
     let res = await fetch(url);
     return await res.json();
+    console.log(res);
   } catch (error) {
     console.log(error);
   }
 }
 
+getData();
 
 */
 
