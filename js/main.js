b7bce813f6db047ec602d0ce163d09270e4c67b6
Avril_M <turvey.am@gmail.com>
2022-08-04 16:26:01 -0500

    Updated Js File

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
-
-// Replace ./data.json with your JSON feed
-
-
-fetch('./data.json').then(response => {
-    return response.json();
-  }).then(data => {
-    // Work with JSON data here
-  console.log(data);  
-   const newData = JSON.parse(data);
-   console.log(newData.title);  
-  }).catch(err => {
-    // Do something for an error here
-  });
-
-
-
 // Global variables
 
 const daily = document.querySelector(".daily");
@@ -23,6 +6,34 @@ const weekly = document.querySelector(".weekly");
 console.log(weekly);
 const monthly = document.querySelector(".monthly");
 console.log(monthly);
+
+// Replace ./data.json with your JSON feed
+
+async function loadData() {
+  fetch('./data.json').then(response => {
+    return response.json();
+  }).then(data => {
+    // Work with JSON data here
+  console.log(data);  
+  }).catch(err => {
+    // Do something for an error here
+  });
+}
+
+loadData();
+
+
+/* I need to create a function that will update the daily info
+by updating the current and previous sections of every section
+Then create an event listener that will call this function when the daily section is clicked. */
+
+
+const dailyInfo = function () {
+  const daily = data.current;
+  return daily;
+}
+
+
 
 daily.addEventListener("click", function () {
 
